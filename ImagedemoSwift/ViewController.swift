0948b89188d448989242c239b0b42d015d593664
NikhilBoriwaleKshatriya <38933949+NikhilBoriwaleKshatriya@users.noreply.github.com>
2018-05-04 17:07:50 +0530

    IMAGE DEMO USING SWIFT
    using UIImagew Picker select image & display image view

--- ImagedemoSwift/ViewController.swift
+++ ImagedemoSwift/ViewController.swift
@@ -8,17 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+    @IBOutlet weak var myimage: UIImageView!
+    @IBAction func btnselectimage(_ sender: UIButton) {
+        
+        let imag = UIImagePickerController()
+        imag.delegate = self
+        imag.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(imag, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        myimage.image = info[UIImagePickerControllerOriginalImage]as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
